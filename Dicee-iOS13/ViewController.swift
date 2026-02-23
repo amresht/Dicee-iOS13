dc85f75eb86ba6e27b5caf101c67554ef76f4ece
amresht <amresh2k@gmail.com>
2026-02-23 23:28:05 +0530

    Load action

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        diceImageViewOne.image = UIImage(imageLiteralResourceName: "DiceSix")
         // Do any additional setup after loading the view.
     }
 
